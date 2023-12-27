cf58facaaddb9d38863f9ba92c17696e93eff4ec
Stiven <69906848+BrayanSRojasG14@users.noreply.github.com>
2023-12-27 08:45:31 -0500

    Organizacion de paginas web y los estilos.

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <h1>Brayan Stiven Rojas Gutierrez</h1>
+    <h2>Analista de sistemas</h2>
+    <img src="https://inesdi-cdn.s3.eu-west-3.amazonaws.com/inesdi-prod/2021-06/inesdi-img-analista-datos-big-data-2048x1366.jpg" alt="" width="1000px">
+    <div class="">
+      Me gusta el desarrollo de software de forma agil ya sea con Yira o Trello, Amante de la programacion en la nube con Amazon y Google.
+    </div>
+    <h3>Tecnologias con las que desarollo</h3>
+    <p>Informacion completa <NuxtLink to="/pages/Tecnologias">Aqui</NuxtLink></p>
+    <img src="https://inesdi-cdn.s3.eu-west-3.amazonaws.com/inesdi-prod/2021-06/inesdi-img-analista-datos-big-data-2048x1366.jpg" alt="" width="100px">
+    <ul>
+      <li>
+        <NuxtLink to="/pages/Proyectos">Proyectos</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/pages/Pruebas">Pruebas</NuxtLink>
+      </li>
+      <li>
+        <NuxtLink to="/pages/Educacion">Educacion</NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
