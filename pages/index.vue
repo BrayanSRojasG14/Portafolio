0583480912ce65bc605199a969a4f77e549eecb3
Stiven <69906848+BrayanSRojasG14@users.noreply.github.com>
2024-01-11 13:22:44 -0500

    Ajuste de estilos y agrego botones de redes sociales.

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
+<script setup lang="ts">
+</script>
 <template>
-  <div class="bg-slate-200">
-    <h1 class="text-3xl text-center">Full stack</h1>
+  <div class="bg-slate-200 dark:bg-slate-800">
     <div class="">
-      <h2>Sobre mi</h2>
+      <h1 class="text-3xl text-center">Brayan Stiven Rojas Gutierrez</h1>
+      <h2 class="text-3 text-center">Full stack con Nuxt y Django</h2>
+      <div class="redes">
+      <button class="px-4 py-1 text-sm text-black font-semibold rounded-full border border-black hover:text-white hover:bg-black hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
+        GitHub
+      </button>
+      <button class="px-4 py-1 text-sm text-blue-500 font-semibold rounded-full border border-blue-400 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
+        Linkedin
+      </button>
+  </div>
+    </div>
+    <div class="h-screen">
+      <h2>Perfil</h2>
       Me gusta el desarrollo de software de forma agil ya sea con Yira o Trello, Amante de la programacion en la nube con Amazon y Google.
     </div>
     <h3>Tecnologias con las que desarollo</h3>
@@ -20,4 +33,10 @@
       <img src="https://inesdi-cdn.s3.eu-west-3.amazonaws.com/inesdi-prod/2021-06/inesdi-img-analista-datos-big-data-2048x1366.jpg" alt="" width="100px">
     </div>
   </div>
+  <div class="pt-1">
+    <button class="px-4 py-1 text-sm text-blue-500 font-semibold rounded-full border border-blue-400 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
+      Descarga CV
+    </button>
+  </div>
+  
 </template>
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./assets/css/*.{html,js}"],
+  "darkMode": 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
