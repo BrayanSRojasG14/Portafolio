c75f9e9520097599c19a486ed945f806635e9446
Stiven <69906848+BrayanSRojasG14@users.noreply.github.com>
2024-01-13 21:35:43 -0500

    Se añadieron explicaciones detalladas de proyectos y nuevos features futuros.

--- pages/index.vue
+++ pages/index.vue
@@ -1,42 +1,56 @@
 <script setup lang="ts">
 </script>
 <template>
-  <div class="bg-slate-200 dark:bg-slate-800">
+  <div class="bg-slate-200 dark:bg-slate-800 ">
     <div class="">
-      <h1 class="text-3xl text-center">Brayan Stiven Rojas Gutierrez</h1>
-      <h2 class="text-3 text-center">Full stack con Nuxt y Django</h2>
-      <div class="redes">
-      <button class="px-4 py-1 text-sm text-black font-semibold rounded-full border border-black hover:text-white hover:bg-black hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
-        GitHub
-      </button>
-      <button class="px-4 py-1 text-sm text-blue-500 font-semibold rounded-full border border-blue-400 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
-        Linkedin
-      </button>
-  </div>
+      <h1 class="text-2xl text-center">Brayan Stiven Rojas Gutierrez</h1>
+      <h2 class="text-xl text-center">Full stack con Nuxt y Django</h2>
+      <div class="text-center">
+        Curriculum
+        <button class="px-4 py-1 text-sm text-black font-semibold rounded-full border border-black hover:text-white hover:bg-black hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
+           Descargar
+        </button>
+      </div>
+      <div class="text-center">
+        Contacto: bsrg2001@hotmail.com
+        <button class="px-4 py-1 text-sm text-black font-semibold rounded-full border border-black hover:text-white hover:bg-black hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
+            Copiar
+        </button>
+      </div>
     </div>
-    <div class="h-screen">
-      <h2>Perfil</h2>
+    <div class="text-center h-screen">
+      <h2 class="text-xl mt-40">Perfil</h2>
       Me gusta el desarrollo de software de forma agil ya sea con Yira o Trello, Amante de la programacion en la nube con Amazon y Google.
     </div>
-    <h3>Tecnologias con las que desarollo</h3>
+    <h1 class="text-3xl text-center">Proyectos</h1>
     <div class="">
-      <p>Informacion completa <NuxtLink to="#">Aqui</NuxtLink></p>
+      <h2 class="text-xl">Clepsidra <NuxtLink to="#">Aqui</NuxtLink></h2>
+      Proyecto blockchain de capa dos que integra una gran cantidad de tecnologias para que el usuario pueda invertir de una forma barata y rapida.
+      Solidity
+      Nuxt
+      tailwindcss
+      typescript
+      AWS
+      Linux
       <img src="https://inesdi-cdn.s3.eu-west-3.amazonaws.com/inesdi-prod/2021-06/inesdi-img-analista-datos-big-data-2048x1366.jpg" alt="" width="100px">
     </div>
-    <h3>Proyectos</h3>
     <div class="">
-      <p>Clepsidra <NuxtLink to="#">Aqui</NuxtLink></p>
+      <h2 class="text-xl">Fundacion Scherman<NuxtLink to="#">Aqui</NuxtLink></h2>
+      Nacio con la idea de tener un sistema optimo para los pacientes y medicos en la palam de su mano y que ayude en la gestion de los mismos.
+      Tecnologias
+      Python
+      Django
+      Typescript
+      css
       <img src="https://inesdi-cdn.s3.eu-west-3.amazonaws.com/inesdi-prod/2021-06/inesdi-img-analista-datos-big-data-2048x1366.jpg" alt="" width="100px">
     </div>
-    <div class="">
-      <p>Fundacion Scherman<NuxtLink to="#">Aqui</NuxtLink></p>
-      <img src="https://inesdi-cdn.s3.eu-west-3.amazonaws.com/inesdi-prod/2021-06/inesdi-img-analista-datos-big-data-2048x1366.jpg" alt="" width="100px">
-    </div>
-  </div>
-  <div class="pt-1">
+    <div class="text-center">
+    <button class="px-4 py-1 text-sm text-black font-semibold rounded-full border border-black hover:text-white hover:bg-black hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
+      GitHub
+    </button>
     <button class="px-4 py-1 text-sm text-blue-500 font-semibold rounded-full border border-blue-400 hover:text-white hover:bg-blue-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
-      Descarga CV
+      Linkedin
     </button>
   </div>
-  
+  </div>
 </template>
