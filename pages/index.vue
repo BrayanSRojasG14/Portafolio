a6187cfef2ca3e9b69203d497fc6b28b2429d1b1
Stiven <69906848+BrayanSRojasG14@users.noreply.github.com>
2024-01-15 21:19:15 -0500

    Cambio de estilos en featuares, se añadio contenido.

--- pages/index.vue
+++ pages/index.vue
@@ -7,20 +7,22 @@
       <h2 class="text-xl text-center">Full stack con Nuxt y Django</h2>
       <div class="text-center">
         Curriculum
-        <button class="px-4 py-1 text-sm text-black font-semibold rounded-full border border-black hover:text-white hover:bg-black hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
+        <button class="px-4 py-1 text-sm text-red-500 font-semibold rounded-full border border-red-500 hover:text-white hover:bg-red-500 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
            Descargar
         </button>
       </div>
       <div class="text-center">
         Contacto: bsrg2001@hotmail.com
-        <button class="px-4 py-1 text-sm text-black font-semibold rounded-full border border-black hover:text-white hover:bg-black hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
+        <button class="px-4 py-1 text-sm text-black font-semibold border border-black hover:text-white hover:bg-black hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
             Copiar
         </button>
       </div>
     </div>
-    <div class="text-center h-screen">
-      <h2 class="text-xl mt-40">Perfil</h2>
-      Me gusta el desarrollo de software de forma agil ya sea con Yira o Trello, Amante de la programacion en la nube con Amazon y Google.
+    <div class="text-center my-96">
+      <h2 class="text-xl">Perfil</h2>
+      Me gusta el desarrollo de software de forma agil ya sea con Yira o Trello. 
+      Amante de la programacion en la nube con Amazon y Google. 
+      Me gusta poner pautas de ciberseguridad. 
     </div>
     <h1 class="text-3xl text-center">Proyectos</h1>
     <div class="">
@@ -32,7 +34,7 @@
       typescript
       AWS
       Linux
-      <img src="https://inesdi-cdn.s3.eu-west-3.amazonaws.com/inesdi-prod/2021-06/inesdi-img-analista-datos-big-data-2048x1366.jpg" alt="" width="100px">
+      <img src="https://inesdi-cdn.s3.eu-west-3.amazonaws.com/inesdi-prod/2021-06/inesdi-img-analista-datos-big-data-2048x1366.jpg" alt="imagen del proyecto Clepsidra" width="500px">
     </div>
     <div class="">
       <h2 class="text-xl">Fundacion Scherman<NuxtLink to="#">Aqui</NuxtLink></h2>
@@ -42,7 +44,11 @@
       Django
       Typescript
       css
-      <img src="https://inesdi-cdn.s3.eu-west-3.amazonaws.com/inesdi-prod/2021-06/inesdi-img-analista-datos-big-data-2048x1366.jpg" alt="" width="100px">
+      <img src="https://inesdi-cdn.s3.eu-west-3.amazonaws.com/inesdi-prod/2021-06/inesdi-img-analista-datos-big-data-2048x1366.jpg" alt="" width="500px">
+    </div>
+    <div class="">
+      <h1 class="text-2xl text-center">Habilidades</h1>
+      Django, Nuxt, Tailwind and AWS.
     </div>
     <div class="text-center">
     <button class="px-4 py-1 text-sm text-black font-semibold rounded-full border border-black hover:text-white hover:bg-black hover:border-transparent focus:outline-none focus:ring-2 focus:ring-offset-2">
