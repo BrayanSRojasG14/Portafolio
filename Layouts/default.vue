cc626c610ac0e75895b89e260cebb61aac649ccf
Stiven <69906848+BrayanSRojasG14@users.noreply.github.com>
2024-02-13 21:35:36 -0500

    Se hicieron cambios en el nav para mirar en deploy.

--- Layouts/default.vue
+++ Layouts/default.vue
@@ -1,21 +1,54 @@
 <template>
-    <div>
-      <nav class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <ul class="retive flex h-16 items-center justify-between">
-          <!--Movil-->
-          <li>
-            <NuxtLink to="#">Proyectos</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="#">Pruebas</NuxtLink>
-          </li>
-          <li>
-            <NuxtLink to="#">Educacion</NuxtLink>
-          </li>
-        </ul>
-      </nav>
-      <div class="">
-        <slot />
-      </div>
+    <div class="bg-white">
+      <header class="absolute inset-x-0 top-0 z-50">
+        <nav class="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+          <div class="flex lg:flex-1">
+            <a href="#" class="-m-1.5 p-1.5">
+              <span class="sr-only">Your Company</span>
+              Portafolio
+            </a>
+          </div>
+          <div class="flex lg:hidden">
+            <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700">
+              <span class="sr-only">Open main menu</span>
+              <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+              </svg>
+            </button>
+          </div>
+          <div class="hidden lg:flex lg:gap-x-12">
+            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Proyectos</a>
+            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Pruebas</a>
+            <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Educacion</a>
+          </div>
+          <div class="lg:hidden" role="dialog" aria-modal="true">
+            <div class="fixed inset-0 z-50"></div>
+            <div class="fixed inset-y-o right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/100">
+              <div class="flex items-center justify-between">
+                <a href="" class="-m-1.5 p-1.5">
+                  <span class="sr-only">Your Company</span>
+                  <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="">
+                </a>
+                <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700">
+                  <span class="sr-only">Close menu</span>
+                  <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-with="1.5" stroke="currentColor" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                  </svg>
+                </button>
+              </div>
+              <div class="mt-6 flow-root">
+                <div class="-my-6 divide-y divide-gray-500/100">
+                  <div class="space-y-2 py-6">
+                    <a href="" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Proyectos</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <div class="">
+          <slot />
+        </div>
+      </header>
     </div>
   </template>  
